ecc805babcf97d4c887f608650f6894ded2d91e2
Fakih <fakihpaesal8@gmail.com>
2025-08-28 09:47:12 +0700

    first commit

---
+++ KULIAH/belajar_dart/bin/latihan/latihan1.dart
@@ -0,0 +1,10 @@
+void main(){
+  String nama = 'Fakih';
+  int umur = 22;
+  num tinggi = 170;
+  bool status_mahasiswa = true;
+  print("Nama saya : $nama");
+  print("Umur saya : $umur tahun");
+  print("Tinggi saya : $tinggi cm");
+  print("Status mahasiswa : $status_mahasiswa");
+}
